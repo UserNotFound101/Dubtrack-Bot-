98db2823db27d11ef6c859889e66269abb56f649
xXSKHXx <salvadin2@gmail.com>
2015-12-18 18:46:12 -0500

    Tbh just some simple updates
    Simple updates :P

--- iBot.js
+++ iBot.js
@@ -1,10 +1,26 @@
 var isIBotRunning;
-
 if(!isIBotRunning) {
-	// Name and Version
-	var NAME = "iBot";
-	var VERSION = "v1.0.0";
-
+	if (typeof Array.prototype.indexOf !== "function") {
+	    Array.prototype.indexOf = function (item) {
+	        for(var i = 0; i < this.length; i++) {
+	            if (this[i] === item) {
+	                return i;
+	            }
+	        }
+	        return -1;
+	    };
+	};
+	if(typeof String.prototype.replaceAll !== "function") {
+		String.prototype.replaceAll = function(oldString, newString) {
+			var theStr = this;
+			var index = theStr.indexOf(oldString);
+			while(index != -1) {
+				theStr = theStr.replace(oldString, newString);
+				index = theStr.indexOf(oldString);
+			}
+			return theStr;
+		};
+	};
 	// Plug.DJ Ported API for Dubtrack.FM
 	API = {
 		getDJ: function() {
@@ -15,20 +31,26 @@ if(!isIBotRunning) {
 		},
 		getRole: function(User) {
 			if(User.attributes.roleid != null) {
-				if(User.attributes.roleid.type == "dj") {
-					return "(Resident?) DJ";
-				}
-				if(User.attributes.roleid.type == "vip") {
+				var type = User.attributes.roleid.type.toLowerCase();
+				switch(type) {
+				case "dj":
+					return "DJ";
+					break;
+				case "vip":
 					return "VIP";
-				}
-				if(User.attributes.roleid.type == "mod") {
+					break;
+				case "mod":
 					return "Moderator";
-				}
-				if(User.attributes.roleid.type == "manager") {
+					break;
+				case "manager":
 					return "Manager";
-				}
-				if(User.attributes.roleid.type == "co-owner") {
+					break;
+				case "co-owner":
 					return "Co-Owner (or Owner)";
+					break;
+				default:
+					return "Regular User";
+					break;
 				}
 			} else {
 				return "Role not found! (Most likely means default user)";
@@ -56,15 +78,14 @@ if(!isIBotRunning) {
 			Dubtrack.Events.unbind(Event, Function);
 		}
 	};
-
 	// Custom stuff
 	IBot = {
-		iBot: NAME + " " + VERSION,
+		iBot: "iBot v1.0.0",
 		Tools: {
-			lookForUser: function(String) {
+			lookForUser: function(username) {
 				var found = false;
-				for(var i = 0; i < $(".username").length; i++) {
-					if(String.toLowerCase() == $(".username")[i].innerHTML.toLowerCase()) {
+				for(var i = 0; i < Dubtrack.room.users.collection.length; i++) {
+					if(username.toLowerCase() == Dubtrack.room.users.collection.at(i).attributes._user.username.toLowerCase()) {
 						found = true;
 					}
 				}
@@ -73,38 +94,24 @@ if(!isIBotRunning) {
 				} else {
 					return false;
 				}
-			},
-			getUsers: function() {
-				var users = "";
-				for(var i = 0; i < $(".username").length; i++) {
-					if(!users.includes($(".username")[i].innerHTML) && $(".username")[i].innerHTML != undefined) {
-						users += "@" + $(".username")[i].innerHTML + " ";
-					}
-				}
-				return users;
 			}
 		}
 	};
-
 	function userJoinMsg(data) {
 		API.sendChat(":wave: Welcome/Welcome back to the room @" + data.user.username + "! :wave:");
 	}
-
-	function userLeaveMsg() {
+	function userLeaveMsg(data) {
 		API.sendChat(":wave: Goodbye @" + data.user.username + "! :wave:");
 	}
-
 	function commandHandler(data) {
 		var msg = data.message;
 		var user = data.user.username;
 		var userId = data.user._id;
-	
-		if(msg.startsWith("!")) {
-			if(msg === "!help") {
-				API.sendChat(IBot.iBot + " user commands: help, cookie @{User}, dj, song, list, autodub");
-			}
-			if(msg.startsWith("!cookie")) {
-				var UN = msg.substring(9);
+		
+		if(msg.substring(0, 1) == "!") {
+			var cmd = msg.substring(1);
+			if(cmd.startsWith("cookie")) {
+				var UN = cmd.substring(8);
 				if(UN != "") {
 					if(IBot.Tools.lookForUser(UN)) {
 						API.sendChat(":cookie: *hands @" + UN + " a cookie, a note on it reads 'With love, from @" + user + "'* :cookie:");
@@ -114,26 +121,30 @@ if(!isIBotRunning) {
 				} else {
 					API.sendChat(":cookie: *hands you a cookie (for @" + user + ")* :cookie:");
 				}
-			}
-			if(msg === "!dj") {
-				API.sendChat("Current DJ: @" + API.getDJ() + "!");
-			}
-			if(msg == "!song") {
-				API.sendChat("Current Song: " + API.getMedia() + "!");
-			}
-			if(msg === "!list") {
-				API.sendChat("Users 'found': " + IBot.Tools.getUsers());
-			}
-			if(msg === "!autodub") {
-				API.sendChat("Recommended Dubtrack.FM Extensions: iWoot: http://xxskhxx.comoj.com/tools.php, MikuPlugin: http://mikuplugin.me, and/or DubX: https://dubx.net");
+			} else {
+				switch (cmd) {
+				case "help":
+					API.sendChat(IBot.iBot + " user commands: help, cookie @{User}, dj, song, list, autodub");
+					break;
+				case "dj":
+					API.sendChat("Current DJ: @" + API.getDJ() + "!");
+					break;
+				case "song":
+					API.sendChat("Current Song: " + API.getMedia() + "!");
+					break;
+				case "autodub":
+					API.sendChat("Recommended Dubtrack.FM Extensions: iWoot: http://xxskhxx.comoj.com/tools.php, MikuPlugin: http://mikuplugin.me, and/or DubX: https://dubx.net");
+					break;
+				default:
+					API.sendChat("Command: " + cmd + ", was not found!");
+					break;
+				}
 			}
 		}
 	}
-
 	function nextSongMsg() {
 		API.sendChat(":musical_note: Now playing: " + API.getMedia() + "! DJ: " + API.getDJ() + ":musical_note:");
 	}
-
 	function connectAPI() {
 		API.on(API.CHAT, commandHandler);
 		API.on(API.USER_JOIN, userJoinMsg);
@@ -143,15 +154,13 @@ if(!isIBotRunning) {
 		* API.on(API.ADVANCE, nextSongMsg);
 		*/
 	}
-
-	// Connect everything on to start up correctly
+	// Just like iWoot, CONNECT EVERYTHING!
 	function startUp() {
 		connectAPI();
 		document.getElementById("chat-txt-message").maxLength = 99999999999999999999;
 		isIBotRunning = true;
 		API.sendChat(IBot.iBot + " Started!");
 	}
-
 	startUp();
 } else {
 	Dubtrack.helpers.displayError("Error!", "iBot is already running!");
